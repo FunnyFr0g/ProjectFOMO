ab67cb7939f172371d831863899fd22b72989d0c
ILYA <i.n.lagutin@yandex.ru>
2025-09-18 13:39:34 +0300

    Убрал лишние файлы

--- scripts/upload edge impulse.py
+++
@@ -1,9 +0,0 @@
-from edgeimpulse_api import IngestionApi
-
-api = IngestionApi(api_key="ei_...")
-response = api.upload_sample(
-    category="training",
-    label="bird",
-    file_path="./dataset/images/09754.jpg"
-)
-print(response)
